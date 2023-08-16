c6018b4d2f51955016c3595c683f034822875dbc
Tomas <tomasloha@gmail.com>
2023-08-15 21:24:04 -0300

    primer commit

---
+++ src/main/java/com/inmueble/InmobiliariaSp/InmobiliariaSpApplication.java
@@ -0,0 +1,13 @@
+package com.inmueble.InmobiliariaSp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmobiliariaSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmobiliariaSpApplication.class, args);
+	}
+
+}
